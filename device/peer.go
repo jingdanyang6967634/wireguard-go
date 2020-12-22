jingdanyang6967634/wireguard-go

commit e739ff71a58599b0e8b2f41a5031ef3cfdde4f89

device: fix persistent_keepalive_interval data races
Co-authored-by: David Anderson <danderson@tailscale.com>
Signed-off-by: Josh Bleecher Snyder <josh@tailscale.com>

device/peer.go
@@ -27,7 +27,7 @@ type Peer struct {
 	handshake                   Handshake
 	device                      *Device
 	endpoint                    conn.Endpoint
-	persistentKeepaliveInterval uint16
+	persistentKeepaliveInterval uint32 // accessed atomically
 	disableRoaming              bool
 
 	// These fields are accessed with atomic operations, which must be

device/device.go
@@ -163,7 +163,7 @@ func deviceUpdateState(device *Device) {
 		device.peers.RLock()
 		for _, peer := range device.peers.keyMap {
 			peer.Start()
-			if peer.persistentKeepaliveInterval > 0 {
+			if atomic.LoadUint32(&peer.persistentKeepaliveInterval) > 0 {
 				peer.SendKeepalive()
 			}
 		}

device/device_test.go
@@ -215,7 +215,20 @@ func TestConcurrencySafety(t *testing.T) {
 	}()
 	warmup.Wait()
 
-	// coming soon: more things here...
+	// Change persistent_keepalive_interval concurrently with tunnel use.
+	t.Run("persistentKeepaliveInterval", func(t *testing.T) {
+		cfg := uapiCfg(
+			"public_key", "f70dbb6b1b92a1dde1c783b297016af3f572fef13b0abb16a2623d89a58e9725",
+			"persistent_keepalive_interval", "1",
+		)
+		for i := 0; i < 1000; i++ {
+			cfg.Seek(0, io.SeekStart)
+			err := pair[0].dev.IpcSetOperation(cfg)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	})
 
 	close(done)
 }

device/timers.go
@@ -138,7 +138,7 @@ func expiredZeroKeyMaterial(peer *Peer) {
 }
 
 func expiredPersistentKeepalive(peer *Peer) {
-	if peer.persistentKeepaliveInterval > 0 {
+	if atomic.LoadUint32(&peer.persistentKeepaliveInterval) > 0 {
 		peer.SendKeepalive()
 	}
 }
@@ -201,8 +201,9 @@ func (peer *Peer) timersSessionDerived() {
 
 /* Should be called before a packet with authentication -- keepalive, data, or handshake -- is sent, or after one is received. */
 func (peer *Peer) timersAnyAuthenticatedPacketTraversal() {
-	if peer.persistentKeepaliveInterval > 0 && peer.timersActive() {
-		peer.timers.persistentKeepalive.Mod(time.Duration(peer.persistentKeepaliveInterval) * time.Second)
+	keepalive := atomic.LoadUint32(&peer.persistentKeepaliveInterval)
+	if keepalive > 0 && peer.timersActive() {
+		peer.timers.persistentKeepalive.Mod(time.Duration(keepalive) * time.Second)
 	}
 }
 

device/uapi.go
@@ -86,7 +86,7 @@ func (device *Device) IpcGetOperation(socket *bufio.Writer) error {
 			send(fmt.Sprintf("last_handshake_time_nsec=%d", nano))
 			send(fmt.Sprintf("tx_bytes=%d", atomic.LoadUint64(&peer.stats.txBytes)))
 			send(fmt.Sprintf("rx_bytes=%d", atomic.LoadUint64(&peer.stats.rxBytes)))
-			send(fmt.Sprintf("persistent_keepalive_interval=%d", peer.persistentKeepaliveInterval))
+			send(fmt.Sprintf("persistent_keepalive_interval=%d", atomic.LoadUint32(&peer.persistentKeepaliveInterval)))
 
 			for _, ip := range device.allowedips.EntriesForPeer(peer) {
 				send("allowed_ip=" + ip.String())
@@ -333,8 +333,7 @@ func (device *Device) IpcSetOperation(r io.Reader) error {
 					return &IPCError{ipc.IpcErrorInvalid}
 				}
 
-				old := peer.persistentKeepaliveInterval
-				peer.persistentKeepaliveInterval = uint16(secs)
+				old := atomic.SwapUint32(&peer.persistentKeepaliveInterval, uint32(secs))
 
 				// send immediate keepalive if we're turning it on and before it wasn't on
 

commit d09620071539c0614f3665002fc35793a4f84ce0

device: fix alignment of peer stats member
This was shifted by 2 bytes when making persistent keepalive into a u32.
Fix it by placing it after the aligned region.

Fixes: e739ff7 ("device: fix persistent_keepalive_interval data races")
Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

device/peer.go
@@ -28,7 +28,6 @@ type Peer struct {
 	device                      *Device
 	endpoint                    conn.Endpoint
 	persistentKeepaliveInterval uint32 // accessed atomically
-	disableRoaming              bool
 
 	// These fields are accessed with atomic operations, which must be
 	// 64-bit aligned even on 32-bit platforms. Go guarantees that an
@@ -40,6 +39,8 @@ type Peer struct {
 		rxBytes           uint64 // bytes received from peer
 		lastHandshakeNano int64  // nano seconds since epoch
 	}
+
+	disableRoaming bool
 
 	timers struct {
 		retransmitHandshake     *Timer